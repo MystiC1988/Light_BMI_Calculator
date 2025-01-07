e8031a770284bee78250c20a2062b690eddccfec
Osvaldo Martinez <osvaldo.1988@gmail.com>
2025-01-07 01:53:42 -0500

    Add Only Portrait configuration for iOS and Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:light_bmi_calculator/presentation/screens/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((_) {
+    runApp(const MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
